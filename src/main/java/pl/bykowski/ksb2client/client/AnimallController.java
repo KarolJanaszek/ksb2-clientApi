3a9910c1f6d526516f0eeb9171d95c7d677124ab
Karol Janaszek <52563745+KarolJanaszek@users.noreply.github.com>
2020-11-28 16:39:06 +0100

    .

--- src/main/java/pl/bykowski/ksb2client/client/AnimallController.java
+++ src/main/java/pl/bykowski/ksb2client/client/AnimallController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
-public class CatController {
+public class AnimallController {
 
-    public CatController() {
+    public AnimallController() {
         RestTemplate restTemplate = new RestTemplate();
         AnimalFact[] forObject = restTemplate.getForObject("https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=2", AnimalFact[].class);
 
